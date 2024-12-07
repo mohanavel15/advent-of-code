9ad92535497be459febb785cab55cfbb89cba75a
Mohan <mohanavel15@protonmail.com>
2024-12-07 22:44:11 +0530

    day07 part 2 solution

--- 2024/src/day07.zig
+++ 2024/src/day07.zig
@@ -54,4 +54,78 @@ pub fn Solution() !void {
     }
 
     std.log.info("Solution 1: Sum of correct equations: {}", .{result_sum});
+
+    var results2 = std.ArrayList(u128).init(allocator);
+    defer results2.deinit();
+
+    lines = std.mem.tokenizeAny(u8, content, "\n");
+    while (lines.next()) |line| {
+        var values = std.mem.tokenizeAny(u8, line, ": ");
+        const result = try parseInt(u128, values.next().?, 10);
+        var operands = std.ArrayList(u128).init(allocator);
+        defer operands.deinit();
+        while (values.next()) |value| {
+            try operands.append(try parseInt(u128, value, 10));
+        }
+
+        const operators = try allocator.alloc(u2, operands.items.len - 1);
+        @memset(operators, 0);
+
+        defer allocator.free(operators);
+
+        const combinations = std.math.pow(usize, 3, operands.items.len - 1);
+        for (0..combinations) |_| {
+            var experimental_result: u128 = operands.items[0];
+            for (0..operators.len) |idx| {
+                switch (operators[idx]) {
+                    0 => experimental_result += operands.items[idx + 1],
+                    1 => experimental_result *= operands.items[idx + 1],
+                    2 => {
+                        // Following is slow.
+                        // const str = try std.fmt.allocPrint(allocator, "{}{}", .{ experimental_result, operands.items[idx + 1] });
+                        // experimental_result = try parseInt(u128, str, 10);
+                        // allocator.free(str);
+
+                        // Inspiration from others.
+                        experimental_result = experimental_result * std.math.pow(u128, 10, std.math.log10_int(operands.items[idx + 1]) + 1) + operands.items[idx + 1];
+                    },
+                    3 => unreachable,
+                }
+            }
+            if (result == experimental_result) {
+                try results2.append(result);
+                break;
+            }
+
+            increment(operators);
+        }
+    }
+
+    var result_sum2: u128 = 0;
+    for (results2.items) |result| {
+        result_sum2 += result;
+    }
+
+    std.log.info("Solution 2: Sum of correct equations: {}", .{result_sum2});
+}
+
+fn increment(values: []u2) void {
+    values[0] += 1;
+    var carry: usize = 0;
+    if (values[0] == 3) {
+        carry += 1;
+        values[0] = 0;
+    }
+
+    var idx: usize = 1;
+    while (idx < values.len and carry > 0) {
+        values[idx] += 1;
+        carry -= 1;
+        if (values[idx] == 3) {
+            values[idx] = 0;
+            carry += 1;
+        }
+
+        idx += 1;
+    }
 }
