3489ea3c0ccde314c928fbfb6452a41dbee480be
Mohan <mohanavel15@protonmail.com>
2025-12-07 14:49:21 +0530

    day 4 - part 2

--- 2025/src/day04.zig
+++ 2025/src/day04.zig
@@ -9,9 +9,6 @@ fn mat_index(row: usize, col: usize) usize {
 }
 
 pub fn Solution() !void {
-    var copy: [DATA.len]u8 = undefined;
-    @memcpy(&copy, DATA);
-
     var accessible: usize = 0;
 
     for (0..HEIGHT) |r| {
@@ -38,10 +35,53 @@ pub fn Solution() !void {
 
             if (sround < 4) {
                 accessible += 1;
-                copy[curr_idx] = 'x';
             }
         }
     }
-    std.debug.print("{s}\n", .{copy});
-    std.debug.print("Day 7 - Solution 1: {d}\n", .{accessible});
+
+    std.debug.print("Day 4 - Solution 1: {d}\n", .{accessible});
+
+    var copy: [DATA.len]u8 = undefined;
+    @memcpy(&copy, DATA);
+
+    accessible = 0;
+
+    while (true) {
+        var accessible_it: usize = 0;
+
+        for (0..HEIGHT) |r| {
+            for (0..WIDTH) |c| {
+                const curr_idx = mat_index(r, c);
+                if (copy[curr_idx] == '.') continue;
+
+                var sround: usize = 0;
+
+                for ([_]isize{ -1, 0, 1 }) |r1| {
+                    for ([_]isize{ -1, 0, 1 }) |c1| {
+                        if (r1 == -1 and r == 0) continue;
+                        if (c1 == -1 and c == 0) continue;
+                        if (r1 == 1 and r >= HEIGHT - 1) continue;
+                        if (c1 == 1 and c >= WIDTH - 1) continue;
+
+                        const idx = mat_index(@intCast(@as(isize, @intCast(r)) + r1), @intCast(@as(isize, @intCast(c)) + c1));
+
+                        if (curr_idx == idx) continue;
+
+                        if (copy[idx] == '@') sround += 1;
+                    }
+                }
+
+                if (sround < 4) {
+                    accessible_it += 1;
+                    copy[curr_idx] = '.';
+                }
+            }
+        }
+
+        if (accessible_it == 0) break;
+
+        accessible += accessible_it;
+    }
+
+    std.debug.print("Day 4 - Solution 2: {d}\n", .{accessible});
 }
